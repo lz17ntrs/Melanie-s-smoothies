5a0f1677f9f9ebdafabd9e6b399b3aa71668deaa
lz17ntrs <lz17@ntrs.com>
2025-08-01 21:38:42 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,5 @@ if ingredients_list:
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.fruityvice.com/api/fruit/watermelon")
-#st.text (fruityvice_response.json())
-sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#st.text (smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
