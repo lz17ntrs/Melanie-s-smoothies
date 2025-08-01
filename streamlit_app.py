1236b5bb7781ab642b132ce76dc5ae7a69163767
lz17ntrs <lz17@ntrs.com>
2025-08-01 19:43:46 +1000

    Update streamlit_app.py
    copied from Custom Smoothies SiS app

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,25 @@ ingredients_list = st.multiselect(
     , max_selections = 5
 )
 
+
+if ingredients_list:
+   
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+            ingredients_string += fruit_chosen + ' '
+
+    #st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    st.write(my_insert_stmt)
+    #st.stop()
+    
+    #st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
